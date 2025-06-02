b05f5d37f3d569b69decd9aa7ae11ed493fee65f
Sebastián Alfonso <joansebas13alfonso@gmail.com>
2025-06-01 20:28:38 -0500

    Configuración para render

--- backend/app.py
+++ backend/app.py
@@ -12,18 +12,17 @@ app = FastAPI(
     title=settings.PROJECT_NAME,
     openapi_url=f"/openapi.json"
 )
+origins = [
+    "https://factura-inventario-k9wymamz8-sebastian-100109s-projects.vercel.app",  #  frontend en Vercel
+    "http://localhost:3000",  # desarrollo local
+]
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=[ "https://factura-inventario.vercel.app",  # Tu dominio de Vercel
-        "http://localhost:3000",  # Para desarrollo local
-        "http://localhost:5173",  # Para desarrollo local con Vite
-                   
-    ],  # Puerto de tu React app
+    allow_origins=origins,  # ⚠️ solo estos dominios podrán hacer peticiones
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
 )
-
 # Incluir rutas
 app.include_router(auth_router)
